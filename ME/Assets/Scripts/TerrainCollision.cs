1eb0c2cf0513dc036182791602e5ca2fb3f55fb5
Samuel Cornforth <s3555445@student.rmit.edu.au>
2016-11-04 20:37:06 +1100

    Destructible earth initial implementation
    May have bugs and/or performance issues

---
+++ ME/Assets/Scripts/TerrainCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainCollision : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		DestroysTerrain destroyer = col.GetComponent<DestroysTerrain> ();
+		if (destroyer != null)
+		{
+			DestructibleTerrain.globalTerrainHandler.collision (new Vector2 (col.transform.position.x, col.transform.position.y), destroyer.destructionRadius, col.gameObject);
+		}
+	}
+}
